4bc8e2782373fe64d4cebaada972ec21501d70d4
arpitgupta <arpitguptajalaun854@gmail.com>
2023-09-15 19:14:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var img=document.getElementById("ticket");
+var text=document.getElementById("demo");
+var button1=document.getElementById("color-change");
+
+function myFunction2(){
+ text.style.display="none";
+ img.style.display="block";
+//  button1.style.backgroundColor="white";
+//  button1.style.Color="black";
+}
+function myFunction(){
+ img.style.display="none";
+ text.style.display="block"; 
+//  button1.style.Color="black";
+}
